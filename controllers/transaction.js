8c2de687de0f2fc8b46f087099698f36a3222175
nGhart <107068059+nGhart@users.noreply.github.com>
2023-10-27 15:30:28 +0000

    Update transaction.js

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -1,18 +1,53 @@
-const transaction = require("../models/transactionModel");
+const Transaction = require("../models/transactionModel");
 
-const newTranasction = async (req, res) => {
+const createTransaction = async (req, res) => {
   const data = req.body;
   try {
-    let newTranaction = await transaction.create(data);
-    if (!newTranaction) {
+    let newTransaction = await Transaction.create(data);
+    if (!newTransaction) {
       return res.status(401).json({ success: false });
     }
-    res.json(newTranaction);
+    res.json(newTransaction);
   } catch (err) {
     res.status(500).send({ message: "Error in saving the transaction" });
   }
 };
 
+const getAllTransactions = async (req, res) => {
+  try {
+    let transactions = await Transaction.find({});
+    return res.json(transactions);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const getSingleAccount = async (req, res) => {
+  const { accountNumber, transactionType } = req.body;
+  try {
+    const query = {};
+
+    if (accountNumber) {
+      query.accountNumber = accountNumber;
+    }
+
+    if (transactionType) {
+      query.transactionType = transactionType;
+    }
+
+    const singleAccount = await Transaction.find(query);
+    if (singleAccount.length === 0) {
+      return res.json({ msg: "No matching records found" });
+    }
+    return res.json(singleAccount);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
 module.exports = {
-  newTranasction,
+  createTransaction,
+  getAllTransactions,
+  getSingleAccount,
 };
